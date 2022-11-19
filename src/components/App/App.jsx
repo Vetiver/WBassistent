8d707ef13d6855aa77d3003ef123c8dc3da1be2d
theverybusybee <v_antonchik@mail.ru>
2022-11-19 20:37:37 +0300

    input field added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Style from "../../components/App/App.module.css";
 import AppHeader from "../../components/AppHeader/AppHeader.jsx";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Modal from "../Modal/Modal.jsx";
 import SpecialOffers from "../SpecialOffers/SpecialOffers";
 import Prices from "../Prices/Prices";
+import SearchField from "../SearchField/SearchField";
+
 function App() {
   const dispanch = useDispatch();
   const IsOpen = useSelector((state) => state.mainReduser.IsOpen);
@@ -14,6 +15,7 @@ function App() {
     <div className={`${Style.App}`}>
       <AppHeader />
       <main className={Style.main}>
+        <SearchField/>
         <SpecialOffers />
         <Prices />
       </main>
