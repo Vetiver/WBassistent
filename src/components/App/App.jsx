0c8b77fd7d4929789d972c756c042262f313f071
Vetiver <dmitriy_serga@mail.ru>
2022-11-22 20:41:20 +0300

    r

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import Style from "../../components/App/App.module.css";
 import AppHeader from "../../components/AppHeader/AppHeader.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Homepage } from "../../pages/all-pages";
 import Footer from "../Footer/Footer";
 
@@ -9,9 +9,11 @@ function App() {
     <div className={`${Style.App}`}>
       <AppHeader />
       <main className={Style.main}>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
+        <Switch>
+          <Route path="/">
+          <Homepage />
+          </Route>
+        </Switch>
       </main>
       <Footer />
     </div>
